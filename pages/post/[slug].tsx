6e2fb66b5dcdceb6916ad79ccb3829b2b001ea8b
narendraktw <narendraktw@gmail.com>
2022-04-07 12:47:52 +0530

    comment section enabled

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -2,13 +2,41 @@ import { GetStaticProps } from 'next'
 import { sanityClient, urlFor } from '../../sanity'
 import { Post } from '../../typings'
 import PortableText from 'react-portable-text'
-
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { useState } from 'react'
 interface Props {
   post: Post
 }
 
+interface IFormInput {
+  _id: string
+  name: string
+  email: string
+  comment: string
+}
+
 function Post({ post }: Props) {
-  console.log(post)
+  const [submitted, setSubmitted] = useState(false)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>()
+
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    fetch('/api/createComment', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    })
+      .then(() => {
+        console.log(data)
+        setSubmitted(true)
+      })
+      .catch((err) => {
+        console.log(err)
+        setSubmitted(false)
+      })
+  }
   return (
     <main>
       <img
@@ -59,36 +87,82 @@ function Post({ post }: Props) {
         </div>
       </article>
       <hr className="my-5 mx-auto max-w-lg border border-yellow-500" />
-      <form className="mx-auto mb-10 flex max-w-2xl flex-col p-5">
-        <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
-        <h4 className="text-3xl font-bold">Leave a comment below!</h4>
-        <hr className="mt-2 py-3" />
-        <label className="mb-5 block">
-          <span className="text-gray-700">Name</span>
+      {submitted ? (
+        <div className="my-10 mx-auto flex max-w-2xl flex-col bg-yellow-500 p-10 text-white">
+          <h3 className="text-3xl font-bold">
+            Thank you for submitting your comment!
+          </h3>
+          <p>Once it has been approved, it will appear below!</p>
+        </div>
+      ) : (
+        <form
+          className="mx-auto mb-10 flex max-w-2xl flex-col p-5"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
+          <h4 className="text-3xl font-bold">Leave a comment below!</h4>
+          <hr className="mt-2 py-3" />
           <input
-            className="form-input mt-1 block w-full rounded border py-2 px-3 shadow ring-yellow-500"
-            placeholder="John Appleseed"
-            type="text"
+            type="hidden"
+            {...register('_id')}
+            name="_id"
+            value={post._id}
           />
-        </label>
-        <label className="mb-5 block">
-          <span className="text-gray-780">Email</span>
-          <input
-            className="form-input mt-1 block w-full rounded border py-2 px-3 shadow ring-yellow-500"
-            placeholder="John Appleseed"
-            type="text"
-          />
-        </label>
+          <label className="mb-5 block">
+            <span className="text-gray-700">Name</span>
+            <input
+              {...register('name', { required: true })}
+              className="form-input mt-1 block w-full rounded border py-2 px-3 shadow ring-yellow-500"
+              placeholder="John Appleseed"
+              type="text"
+            />
+          </label>
+          <label className="mb-5 block">
+            <span className="text-gray-780">Email</span>
+            <input
+              {...register('email', { required: true })}
+              className="form-input mt-1 block w-full rounded border py-2 px-3 shadow ring-yellow-500"
+              placeholder="John Appleseed"
+              type="text"
+            />
+          </label>
 
-        <label className="mb-5 block">
-          <span className="text-gray-700">Comment</span>
-          <textarea
-            className="form-input mt-1 block w-full rounded border py-2 px-3 shadow ring-yellow-500"
-            placeholder="John Appleseed"
-            rows={8}
+          <label className="mb-5 block">
+            <span className="text-gray-700">Comment</span>
+            <textarea
+              {...register('comment', { required: true })}
+              className="form-input mt-1 block w-full rounded border py-2 px-3 shadow ring-yellow-500"
+              placeholder="John Appleseed"
+              rows={8}
+            />
+          </label>
+          <div className="flex flex-col p-5">
+            {errors.name && <p className="text-red-500">Name is required</p>}
+            {errors.email && <p className="text-red-500">Email is required</p>}
+            {errors.comment && (
+              <p className="text-red-500">Comment is required</p>
+            )}
+          </div>
+          <input
+            type="submit"
+            className="focus:shadow-outline cursor-pointer rounded bg-yellow-500 py-2 px-4 font-bold text-white shadow hover:bg-yellow-400 focus:outline-none"
           />
-        </label>
-      </form>
+        </form>
+      )}
+
+      {/* Comment */}
+      <div className="my-5 mx-auto flex max-w-2xl flex-col space-y-2 p-10 shadow shadow-yellow-500">
+        <h3>Comments</h3>
+        <hr />
+        {post.comments.map((comment) => (
+          <div key={comment._id}>
+            <p>
+              <span className="text-yellow-500"> {comment.name}:</span>{' '}
+              {comment.comment}
+            </p>
+          </div>
+        ))}
+      </div>
     </main>
   )
 }
--- sanitymediumclone/schemas/schema.js
+++ sanitymediumclone/schemas/schema.js
@@ -9,7 +9,7 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
-
+import comment from './comment'
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
@@ -25,5 +25,6 @@ export default createSchema({
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
     blockContent,
+    comment,
   ]),
 })
