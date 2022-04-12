cc9ff0ff8f9012c91c0af87489c8c9c76c31ebdc
narendraktw <narendraktw@gmail.com>
2022-04-12 16:35:55 +0530

    authentication

---
+++ components/BtnLogin.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { signIn } from 'next-auth/react'
+
+const BtnLogin = ({ provider, bgColor, txtColor }) => {
+  return (
+    <div>
+      <button
+        className="btn w-100 my-2 py-3"
+        style={{ background: `${bgColor}`, color: `${txtColor}` }}
+        onClick={() => signIn(provider.id)}
+      >
+        Sign in with {provider.name}
+      </button>
+    </div>
+  )
+}
+
+BtnLogin.defaultProps = {
+  txtColor: '#eee',
+}
+export default BtnLogin
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,8 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
-
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
+import clientPromise from './lib/mongodb'
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -15,4 +16,8 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  secret: process.env.JWT_SECRET,
+  adapter: MongoDBAdapter(clientPromise),
+
+  //debug: true,
 })
