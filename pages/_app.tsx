903fa4e6830bcc49c93ec2280d02f9a315df0733
Bisht <narendra.bisht@capgemini.com>
2022-04-13 12:54:57 +0530

    delete .yml file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import { SessionProvider } from 'next-auth/react'
 import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
